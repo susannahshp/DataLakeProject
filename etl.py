1aff123e1656a2f08b62e2e473c7dd0676960891
Susannah Park <susannahshp@gmail.com>
2020-09-13 22:32:52 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -48,7 +48,7 @@ def process_song_data(spark, input_data, output_data):
     song_data = os.path.join(input_data, "song_data/A/*/*/*.json")
     
     # read song data file
-    df = spark.read.json(song_data)
+    df = spark.read.json(song_data).dropDuplicates()
 
     # extract columns to create songs table
     songs_table = df.select("song_id","title","artist_id","year","duration")
@@ -78,7 +78,7 @@ def process_log_data(spark, input_data, output_data):
     log_data = os.path.join(input_data,"log-data/2018/*/*.json")
 
     # read log data file
-    df = spark.read.json(log_data)
+    df = spark.read.json(log_data).dropDuplicates()
     
     # filter by actions for song plays
     df = df.filter(df.page == 'NextSong')
@@ -104,10 +104,10 @@ def process_log_data(spark, input_data, output_data):
                            date_format('timestamp','E').alias('weekday'))
     
     # write time table to parquet files partitioned by year and month
-    time_table.write.mode('overwrite').parquet(os.path.join(output_data, 'time_table'))
+    time_table.write.mode('overwrite').partitionBy('year','month').parquet(os.path.join(output_data, 'time_table'))
 
     # read in song data to use for songplays table
-    song_df = spark.read.json(song_data)
+    song_df = spark.read.json(song_data).dropDuplicates()
     
     # extract columns from joined song and log datasets to create songplays table 
     songplays_table = df.join(song_df, df.song==song_df.title, how='inner')\
